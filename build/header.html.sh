78cc6e0a85c8c0cf9718f29757b8e07aedac311c
Tudor <xenogenesis@openmailbox.org>
2016-08-04 16:00:33 +0300

    blog looks awesome

--- build/header.html.sh
+++ build/header.html.sh
@@ -5,6 +5,8 @@ cat << EOF
     <title>${title}</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Tudor Roman">
+    <meta name="description" content="Tudor's Swamp">
     <link rel="stylesheet" href="/css/main.css">
   </head>
   <body>
